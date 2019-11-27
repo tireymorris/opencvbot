a97407aa18770216bf5cfd45ffc23ccc933d49d7
Ty Morris <tireymorris@gmail.com>
2019-11-27 10:45:59 -0600

    Update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-yarn start &
-source venv/bin/activate
-xvfb-run python3 src/detect_motion.py
+yarn build && yarn start &  source venv/bin/activate && nohup xvfb-run python3 src/detect_motion.py &
